14be281f7281b089d6f20f23d4f0275574d7a746
dcxrhonxai <delacruz.ronalddc@gmail.com>
2025-10-09 19:28:30 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,25 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.fcbb65b390f2441890e8a8de327e1a60',
-  appName: 'sagip-ph-app',
-  webDir: 'dist'
+  appId: 'com.wxtech.emergencyresponseph',
+  appName: 'Emergency Response PH',
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      // How long to display the splash screen in milliseconds
+      launchShowDuration: 3000, 
+      // Whether to auto hide the splash screen
+      launchAutoHide: true,
+      // The background color of the splash screen
+      backgroundColor: "#ffffff", 
+      // Set the scale type of the image (Android only)
+      androidScaleType: "CENTER_CROP",
+      // (Optional) Show a spinner on top of the splash screen
+      showSpinner: true,
+      // (Optional) Color of the spinner
+      spinnerColor: "#999999", 
+    },
+  },
 };
 
 export default config;
