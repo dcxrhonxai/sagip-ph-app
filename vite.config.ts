3f5c888e73f771f9ccbab5f259fd51782554da44
dcxrhonxai <delacruz.ronalddc@gmail.com>
2025-10-10 05:24:40 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from '@tailwindcss/vite'; // Import the new Tailwind CSS plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss() // Add the new Tailwind CSS plugin here
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
