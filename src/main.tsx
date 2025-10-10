c1729282927205fe99393e1dba952a712f47d3ca
dcxrhonxai <delacruz.ronalddc@gmail.com>
2025-10-10 16:45:47 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Plugins } from "@capacitor/core";
+import { initPushNotifications } from './services/pushService';
+
+initPushNotifications();
+
 
 const { PushPlugin } = Plugins;
 
