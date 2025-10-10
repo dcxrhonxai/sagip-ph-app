244eee9eb02d9bdedf58f69d0e1730de50d588d7
dcxrhonxai <delacruz.ronalddc@gmail.com>
2025-10-10 15:46:32 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Plugins } from "@capacitor/core";
 
+const { PushPlugin } = Plugins;
+
+// Listen for push notifications
+PushPlugin.addListener("pushNotificationReceived", (notification) => {
+  console.log("Received push notification:", notification);
+});
+
+// Render your app
 createRoot(document.getElementById("root")!).render(<App />);
