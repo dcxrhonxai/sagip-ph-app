ee6d1f76513b5d4d2d365cc6e26360bac30badfb
delacruzronalddc <delacruz.ronalddc@gmail.com>
2025-10-11 22:06:44 +0800

    Update

--- android/app/src/main/java/app/lovable/fcbb65b390f2441890e8a8de327e1a60/MainActivity.java
+++ android/app/src/main/java/app/lovable/fcbb65b390f2441890e8a8de327e1a60/MainActivity.java
@@ -1,5 +1,22 @@
 package app.lovable.fcbb65b390f2441890e8a8de327e1a60;
 
+import android.Manifest;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                },
+                1001
+        );
+    }
+}
